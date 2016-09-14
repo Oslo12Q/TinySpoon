f9b5a21eb6c25e3addfed8876ef5244c508961c7
CodeMeow <codemeow@yahoo.com>
2016-09-14 14:04:59 +0800

    Add Tags RESTfull interface

--- childrenrecipe/urls.py
+++ childrenrecipe/urls.py
@@ -7,11 +7,10 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 router.register(r'category',views.CategoryViewSet)
 router.register(r'material',views.MaterialViewSet)
-router.register(r'tags',views.TagViewSet)
 router.register(r'procedure',views.ProcedureViewSet)
 router.register(r'recipes',views.RecipeViewSet)
 urlpatterns = [
         url(r'^', include(router.urls)),
-        url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+	url(r'^tags$', views.tags, name='tags'),
 ]
 
--- childrenrecipe/views.py
+++ childrenrecipe/views.py
@@ -5,6 +5,18 @@ from childrenrecipe.serializers import UserSerializer, GroupSerializer
 from .models import *
 from .serializers import *
 from rest_framework.views import APIView
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import (
+	AllowAny,
+	IsAuthenticated
+)
+from rest_framework.decorators import (
+	api_view,
+	permission_classes,
+	parser_classes,
+)
 #from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
 
@@ -49,4 +61,69 @@ class ProcedureViewSet(viewsets.ModelViewSet):
 class TagViewSet(viewsets.ModelViewSet):
 	queryset = Tag.objects.all()
 	serializer_class = TagSerializer
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def tags(request):
+
+	# data = [
+	# 	    {
+	# 	        "category": "abc",
+	# 	        "tags": [
+	# 	            {
+	# 	                "id": 1,
+	# 	                "tag": "bcd"
+	# 	            },
+	# 	            {
+	# 	                "id": 2,
+	# 	                "tag": "dce"
+	# 	            },
+	# 	            {
+	# 	                "id": 3,
+	# 	                "tag": "cef"
+	# 	            }
+	# 	        ]
+	# 	    }
+	# ]
+	import pdb
+	pdb.set_trace()
+
+	data = []
+	categorys = {}
+	
+	tags = Tag.objects.all()
+
+	for tag in tags:
+		tag_id = tag.id
+		tag_name = tag.name
+		category_name = tag.category.name
+		categroy = None
+		if category_name in categorys:
+			category = categorys[category_name]
+		else:
+			category = {'category': category_name, 'tags': []}
+			categorys[category_name] = category
+			data.append(category)
+		category['tags'].append({
+			'id': tag_id,
+			'tag': tag_name
+		})
+	
+	pdb.set_trace()
+
+
+	return Response(data, status=status.HTTP_200_OK)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
