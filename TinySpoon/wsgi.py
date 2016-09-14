a48115e659593a8f4ad4f313a5f7f5fa5b9969c0
CodeMeow <codemeow@yahoo.com>
2016-09-14 13:25:40 +0800

    Update dir struct

--- TinySpoon/wsgi.py
+++ TinySpoon/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for FreeSpoon_ project.
+WSGI config for TinySpoon project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FreeSpoon_.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TinySpoon.settings")
 
 application = get_wsgi_application()
