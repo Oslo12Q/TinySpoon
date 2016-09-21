6a6f84aa372056b7bd6773ed7d5bb7c534d2bc7b
root <root@bogon.localdomain>
2016-09-21 16:26:22 +0800

    Add recommend

--- childrenrecipe/views.py
+++ childrenrecipe/views.py
@@ -3,8 +3,11 @@
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
+import datetime
+from django.utils.timezone import UTC
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import viewsets
 from childrenrecipe.serializers import *
 from .models import *
@@ -143,16 +146,17 @@ class TagViewSet(viewsets.ModelViewSet):
 def tags(request):
 	data = []
 	categorys = {}
-	tags = Tag.objects.all()
+	tags = Tag.objects.exclude(category__is_tag= 1 )
 	for tag in tags:
 		tag_id = tag.id
 		tag_name = tag.name
 		category_name = tag.category.name
+		category_seq = tag.category.seq
 		categroy = None
 		if category_name in categorys:
 			category = categorys[category_name]
 		else:
-			category = {'category': category_name, 'tags': []}
+			category = {'seq': category_seq, 'category': category_name, 'tags': []}
 			categorys[category_name] = category
 			data.append(category)
 		category['tags'].append({
@@ -166,7 +170,11 @@ def tags(request):
 def recipe(request):
         data = []
         tags = {}
-        recipes = Recipe.objects.all()
+	tag = request.GET.get('tag',None)
+	if tag is None:
+		recipes = Recipe.objects.all()
+	else:
+        	recipes = Recipe.objects.filter(tag__name = tag)
         recipes = Paginator(recipes,10)
         page = request.GET.get('page',1)
         try:
@@ -183,10 +191,7 @@ def recipe(request):
                 recipe_exihibitpic = recipe.exihibitpic
                 recipe_introduce = recipe.introduce
 		recipe_tips = recipe.tips
-	
-
                 tag_name = recipe.tag.filter(category__is_tag= 1 )[0].name
-
                 tag = None
                 if tag_name in tags:
                         tag = tags[tag_name]
@@ -203,7 +208,7 @@ def recipe(request):
 			'tips':recipe_tips,
                         'exihibitpic':"http://"+request.META['HTTP_HOST']+recipe_exihibitpic.url,
                         'introduce':recipe_introduce,
-                         'tag': [{"category_name": x.category.name, 'name': x.name}for x in recipe.tag.all()]
+                        'tag': [{"category_name": x.category.name, 'name': x.name}for x in recipe.tag.all()]
                 })
         return Response(data, status=status.HTTP_200_OK)
 
@@ -231,3 +236,69 @@ def tagshow(request):
                         'tag': tag_name
                 })
         return Response(data, status=status.HTTP_200_OK)
+
+
+#@api_view(['GET'])
+#@permission_classes([AllowAny])
+#def get_search(request):	
+#	recipe_name = request.GET.get('recipe_name',None)
+#	recipes = Recipe.objects.filter(name = recipe_name)
+#	data = []
+#        tag ={'recipes': []}
+#        for recipe in recipes:
+#                recipe_id = recipe.id
+#                recipe_create_time = recipe.create_time
+#                recipe_name = recipe.name
+#                recipe_user = recipe.user
+#                recipe_exihibitpic = recipe.exihibitpic
+#                recipe_introduce = recipe.introduce
+#                tag_name=recipe.tag.values()[0]
+#                tag['recipes'].append({
+#                        'id':recipe_id,
+#			'url':"http://"+request.META['HTTP_HOST']+'/'+'api'+'/'+'recipes'+'/'+str(recipe_id),
+#                        'create_time':recipe_create_time,
+#                        'recipe':recipe_name,
+#                        'user':recipe_user,
+#                        'exihibitpic':"http://"+request.META['HTTP_HOST']+recipe_exihibitpic.url,
+#                        'introduce':recipe_introduce,
+#                        'tag': [{"category_name": x.category.name, 'name': x.name}for x in recipe.tag.all()],
+#                })
+#        data.append(tag)
+#        return Response(data,status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def recommend(request):
+        now = datetime.datetime.now()
+        epoch = datetime.datetime(1970, 1, 1, tzinfo=None).replace(tzinfo=None)
+
+	if Recommend.objects.all().filter(pubdate__lte=now): 
+                recommend = Recommend.objects.all().filter(pubdate__lte=now).order_by('-pubdate').first()
+                recommend_image = recommend.image.url
+                recommend_pubdate = recommend.pubdate
+                recommend_create_time = recommend.create_time
+                recommend_recipe_id = recommend.recipe.id
+                recommend_recipe_create_time = recommend.recipe.create_time
+                recommend_recipe_name = recommend.recipe.name
+                recommend_recipe_user = recommend.recipe.user
+                recommend_recipe_introduce = recommend.recipe.introduce
+
+		td = recommend_recipe_create_time - epoch
+                td1 = recommend_create_time - epoch
+                td2 = recommend_pubdate - epoch
+		timestamp_recipe_createtime = int(td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6)
+                timestamp_createtime = int(td1.microseconds + (td1.seconds + td1.days * 24 * 3600) * 10**6)
+                timestamp_pubdate = int(td2.microseconds + (td2.seconds + td2.days * 24 * 3600) * 10**6)
+                recommend = {'recommend_recipe': 'Today\'s Specials', 'create_time': timestamp_createtime,
+                        'pubdate': timestamp_pubdate, 'image': "http://"+request.META['HTTP_HOST']+recommend_image, 'recipe': {}}
+                recommend['recipe'] = {
+                        'id': recommend_recipe_id,
+                        'create_time': timestamp_recipe_createtime,
+                        'name': recommend_recipe_name,
+                        'user': recommend_recipe_user,
+                        'introduce': recommend_recipe_introduce
+                }
+                return Response(recommend, status=status.HTTP_200_OK)
+        else:
+                recommend = {}
+                return Response(recommend, status=status.HTTP_404_NOT_FOUND)
